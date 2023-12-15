65870b9f8cb6137fc343322770f8e491a3c3f59c
noah-d-whitney <me@noahwhitney.com>
2023-12-15 03:17:06 -0500

    Account Register Endpoint

--- ScoreTableApi/Controllers/AccountController.cs
+++ ScoreTableApi/Controllers/AccountController.cs
@@ -11,16 +11,14 @@ namespace ScoreTableApi.Controllers;
 public class AccountController : ControllerBase
 {
     private readonly UserManager<User> _userManager;
-    private readonly SignInManager<User> _signInManager;
+    // private readonly SignInManager<User> _signInManager;
     private readonly ILogger<AccountController> _logger;
     private readonly IMapper _mapper;
 
-    public AccountController(UserManager<User> userManager,
-        SignInManager<User> signInManager, ILogger<AccountController> logger,
+    public AccountController(UserManager<User> userManager, ILogger<AccountController> logger,
         IMapper mapper)
     {
         _userManager = userManager;
-        _signInManager = signInManager;
         _logger = logger;
         _mapper = mapper;
     }
@@ -38,9 +36,17 @@ public class AccountController : ControllerBase
         try
         {
             var user = _mapper.Map<User>(userDto);
-            var result = await _userManager.CreateAsync(user);
+            user.UserName = userDto.Email;
+            var result = await _userManager.CreateAsync(user, userDto.Password);
 
-            if (!result.Succeeded) return BadRequest("User Registration Attempt Failed");
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(error.Code, error.Description);
+                }
+                return BadRequest(ModelState);
+            }
 
             return Accepted();
         }
@@ -52,30 +58,30 @@ public class AccountController : ControllerBase
         }
     }
 
-    [HttpPost]
-    [Route("Login")]
-    [ProducesResponseType(StatusCodes.Status202Accepted)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    public async Task<IActionResult> Login([FromBody] UserLoginDto userDto)
-    {
-        _logger.LogInformation($"Login attempt for {userDto.Email}");
-        if (!ModelState.IsValid) return BadRequest(ModelState);
-
-        try
-        {
-            var result = await _signInManager.PasswordSignInAsync(userDto
-                .Email, userDto.Password, false, false);
-
-            if (!result.Succeeded) return Unauthorized(userDto);
-
-            return Accepted();
-        }
-        catch (Exception ex)
-        {
-            _logger.LogError(ex, $"Something went wrong in the {nameof(Login)}");
-            return Problem($"Something went wrong in the {nameof(Login)}",
-                statusCode: 500);
-        }
-    }
+    // [HttpPost]
+    // [Route("Login")]
+    // [ProducesResponseType(StatusCodes.Status202Accepted)]
+    // [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    // [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    // public async Task<IActionResult> Login([FromBody] UserLoginDto userDto)
+    // {
+    //     _logger.LogInformation($"Login attempt for {userDto.Email}");
+    //     if (!ModelState.IsValid) return BadRequest(ModelState);
+    //
+    //     try
+    //     {
+    //         var result = await _signInManager.PasswordSignInAsync(userDto
+    //             .Email, userDto.Password, false, false);
+    //
+    //         if (!result.Succeeded) return Unauthorized(userDto);
+    //
+    //         return Accepted();
+    //     }
+    //     catch (Exception ex)
+    //     {
+    //         _logger.LogError(ex, $"Something went wrong in the {nameof(Login)}");
+    //         return Problem($"Something went wrong in the {nameof(Login)}",
+    //             statusCode: 500);
+    //     }
+    // }
 }
--- ScoreTableApi/Dto/UserDto.cs
+++ ScoreTableApi/Dto/UserDto.cs
@@ -2,11 +2,19 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ScoreTableApi.Dto;
 
-public class UserDto : UserLoginDto
+public class UserDto
 {
+    [Required]
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; set; }
+
+    [Required]
+    [StringLength(15, ErrorMessage = "Your password is limited from {2} to {1} characters", MinimumLength = 5)]
+    public string Password { get; set; }
+
     public string FirstName { get; set; }
     public string LastName { get; set; }
 
     [DataType(DataType.PhoneNumber)]
-    public string PhoneNumber { get; set; }
+    public string? PhoneNumber { get; set; }
 }
