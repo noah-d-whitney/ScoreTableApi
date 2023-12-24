84e291f383b2c1c7b137939ed66fb38f476512b4
noah-d-whitney <me@noahwhitney.com>
2023-12-24 08:10:57 -0500

    Getuserdata

--- ScoreTableApi/Controllers/ScoreTableApiController.cs
+++ ScoreTableApi/Controllers/ScoreTableApiController.cs
@@ -1,7 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using ScoreTableApi.Dto;
 using ScoreTableApi.Models;
+using ScoreTableApi.Services;
 
 namespace ScoreTableApi.Controllers;
 
@@ -10,10 +13,12 @@ namespace ScoreTableApi.Controllers;
 public class ScoreTableApi : ControllerBase
 {
     private readonly SignInManager<User> _signInManager;
+    private readonly IUserService _userService;
 
-    public ScoreTableApi(SignInManager<User> signInManager)
+    public ScoreTableApi(SignInManager<User> signInManager, IUserService userService)
     {
         _signInManager = signInManager;
+        _userService = userService;
     }
 
     [HttpPost]
@@ -28,5 +33,29 @@ public class ScoreTableApi : ControllerBase
         }
 
         return Unauthorized();
+    }
+
+    [Authorize]
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    [Route("/userData")]
+    public async Task<IActionResult> GetUserData()
+    {
+        try
+        {
+            //Todo Handle errors
+            var user = await _userService.GetUserData();
+
+            if (user is null) return NotFound("Could not get user data!");
+
+            return Ok(user);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500,
+                "Internal Sever Error. Please try Again Later.");
+        }
     }
 }
